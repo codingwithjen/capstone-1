684fc9b3943910d4669ab886151bce9937804499
jen f <77820429+jenfausto@users.noreply.github.com>
2021-12-05 18:58:47 -0700

    update forms

--- forms.py
+++ forms.py
@@ -1,18 +1,25 @@
 from flask_wtf import FlaskForm
 from wtforms import StringField, PasswordField, SubmitField
-from wtforms.validators import InputRequired, EqualTo, Email, Optional, Length
+from wtforms.validators import InputRequired, Email, Optional, Length
+# from models import User?
 
 class SignupForm(FlaskForm):
     """Form for creating a new user profile."""
 
+    username = StringField("Username", validators=[InputRequired(message="Please enter a desired username."), Length(min=3, max=30)],)
+    email = StringField("Email", validators=[InputRequired(message="Please enter a valid email address"), Email()],)
+    password = PasswordField("Password", validators=[Length(min=6, max=30)],)
+
 class LoginForm(FlaskForm):
     """Login form for existing/registered users."""
+
+    username = StringField("Username", validators=[InputRequired(message="Please enter your username")],)
+    password = PasswordField("Password", validators=[InputRequired()],)
 
 class WeatherForm(FlaskForm):
     """User/Anon-User to submit the city they want data from."""
     city = StringField("City", validators=[Optional()])
     zipcode = StringField("Zip Code", validators=[Optional(), Length(min=5, max=15)])
-    # submit = SubmitField("Submit")
 
 class BookmarkForm(FlaskForm):
     """Gives logged in user the ability to bookmark a city so
