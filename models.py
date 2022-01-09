6d76f37f2ab3ea61276722bb6709228d251f2b0a
jen f <77820429+jenfausto@users.noreply.github.com>
2022-01-09 16:47:18 -0700

    update cls authenticate

--- models.py
+++ models.py
@@ -35,7 +35,7 @@ class User(db.Model, UserMixin):
         return f"<User #{self.id}: {self.username}, {self.email}>"
 
     @classmethod
-    def signup(cls, username, pwd):
+    def signup(cls, username, password):
         """Signup user with hashed password and return user."""
 
         hashed_pwd = bcrypt.generate_password_hash(password).decode('UTF-8')
