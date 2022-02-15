75115becb182fef3fb017a9414e6f881a33432f8
jen f <77820429+jenfausto@users.noreply.github.com>
2022-02-14 20:19:31 -0700

    changed db url name

--- app.py
+++ app.py
@@ -19,7 +19,7 @@ app = Flask(__name__)
 
 # Get DB_URI from environ variable (useful for production/testing) or,
 # if not set there, use development local db.
-app.config['SQLALCHEMY_DATABASE_URI'] = (os.environ.get('DATABASE_URL', 'postgresql:///weather'))
+app.config['SQLALCHEMY_DATABASE_URI'] = (os.environ.get('DATABASE_URL', 'postgresql:///weatherflasksearch'))
 
 # the toolbar is only enabled in debug mode: set to True
 app.config['DEBUG_TB_INTERCEPT_REDIRECTS'] = False
