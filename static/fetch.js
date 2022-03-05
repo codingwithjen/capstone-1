5d95dceb4f8c91044392cae3e1c77a9fc9b274cd
jen f <77820429+jenfausto@users.noreply.github.com>
2022-03-04 21:51:56 -0700

    added loading button and updated jQuery ajax

--- static/fetch.js
+++ static/fetch.js
@@ -11,59 +11,33 @@ $(document).ready(function() {
             },
             type: 'POST',
             url: '/fetch',
+            beforeSend: function() {
+                // Show image container
+                $("#loader").show();
+            },
             success: function(data) {
                 if (data.error) {
                     alert(data.error);
                 }
                 else {
                     console.log(data)
+                    // Hide image container
+                    $("#loader").hide();
                     var template = $("#result_template").html();
                     var html = Mustache.render(template, data);
                     $("#result").html(html);
                     $("#flashMessage").hide();
-                    console.log(data);
+                    console.log(data)
                 }
             }
         });
     });
 });
 
-// $(document).ready(function(){
-//     var cities=[];
-//     function loadCities(){
-//      $.getJSON('/cities', function(data, status, xhr){
-//       for (var i = 0; i < data.length; i++ ) {
-//        cities.push(data[i].name);
-//       }
-//     });
-//     };
-//     loadCities();
-   
-//     $('#city').autocomplete({
-//      source: cities, 
-//     }); 
-   
-//     $('form').on('submit', function(e){
-//      $.ajax({
-//       data: {
-//        city:$('#city').val()
-//       },
-//       type: 'POST',
-//       url : '/process'
-//      })
-//      .done(function(data){ 
-//       if (data.error){
-//        $('#result').text(data.error).show();
-//       }
-//       else {
-//        $('#result').html(data.city).show()
-//       }
-//      })
-//      e.preventDefault();
-//     });
-//    }); 
+// 
 
 // $(document).ready(function() {
+    
 //     $("form").on('submit', function(e) {
         
 //         e.preventDefault();
@@ -91,4 +65,3 @@ $(document).ready(function() {
 //         });
 //     });
 // });
-
